7c7ba798f5d4a718f2829961c2a70e8f9e98da9f
Muraoka Naoki <naoki770915@gmail.com>
2021-01-08 22:24:35 +0900

    templateフィルターの作成，関数を組んでそれをtemplateフィルターとしてHTMLファイルに記述した

--- templates/app.py
+++ templates/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.template_filter('born_year')
+def calcurate_born_year(age):
+    now_timestamp = datetime.now()
+    return str(now_timestamp.year - int(age)) + "年"
 
 @app.route('/')
 def index():
@@ -14,13 +20,18 @@ def home(user_name, age):
     }
     return render_template('home.html', login_user = login_user)
 
+class UserInfo:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
 @app.route('/userlist')
 def user_list():
     users = [
-    'Taro', 'Jiro', 'Tom', 'Naoki'
+        UserInfo("Naoki", 23), UserInfo("Taro", 21), UserInfo("Tom", 42), UserInfo("Bob", 56)
     ]
     is_login = True
     return render_template('userlist.html', users = users, is_login = is_login)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = "True")
